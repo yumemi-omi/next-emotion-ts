70bcb5b37a6472d4499d70c1d239637b8a55b9ed
yumemi-omi <hiro.041310402.kt@gmail.com>
2020-11-16 00:28:01 +0900

    chore: import ress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyles } from '@/styles/Globals';
 import type { AppProps } from 'next/app';
 import React from 'react';
+import 'ress';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
